efbd2798151db8bcbd2a3abb911865ccc88bb77b
Dhananjayan23 <dhanasurvivor@gmail.com>
2022-08-14 21:25:45 +0530

    completed 14 problems

--- loopsets/Program.cs
+++ loopsets/Program.cs
@@ -53,6 +53,157 @@ namespace loopsets
             //    a = a + 2;
             //}
             #endregion
+            #region Question5
+            
+            
+            //for (int m = GetInteger("Enter starting number"), n = GetInteger("Enter ending number"); m < n; m++)
+            //{
+            //    if (m % 2 != 0)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(m);
+            //}
+            #endregion
+            #region Question6
+
+            //for (int m = GetInteger("Enter starting number"), n = GetInteger("Enter ending number"); m < n; m++)
+            //{
+            //    if (m % 2== 0)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(m);
+            //}
+            #endregion
+            #region Question7
+
+            //int i = GetInteger("Enter divisible number");
+            //for (int m = 1, n = 100; m <= n; m++)
+            //{
+                
+            //    if (m % i != 0)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine("The numbers that are divisible by " + i + " are : " + m);
+            //}
+            #endregion
+            #region Question8
+
+            //int m = GetInteger("Enter the number");
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    Console.WriteLine(m + "x" + i + "=" + m*i);
+            //}
+            #endregion
+            #region Question9
+
+            //Console.WriteLine("Multiplication table for 1 to 5 upto 20 is : ");
+            //for( int m = 1 , n=20 ; m <=n; m++)
+            //{
+            //    Console.WriteLine(m*1 + "      " + m*2 + "       " + m*3 + "       " + m*4 + "       " + m*5 + "       "  ); ;
+            //}
+            #endregion
+            #region Question10
+
+            //int sum = 0;
+            //int n = GetInteger("Enter the number");
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    sum += i;
+
+            //}
+            //Console.WriteLine("The sum of all the numbers between 1 to " + n + " = " + sum);
+            #endregion
+            #region Question11
+
+            //int sum = 0;
+            //int a = 0;
+            //int n = GetInteger("Enter a number");
+            //while (a <= n)
+            //{
+            //    Console.WriteLine("The sum of all th even numbers from " + a + " to " + n + " = "  + sum);
+            //    a = a + 2;
+            //    sum += a;
+            //}
+            #endregion
+            #region Question12
+
+            //int sum = 1;
+            //int a = 1;
+            //int n = GetInteger("Enter a number");
+            //while (a <= n)
+            //{
+            //    Console.WriteLine("The sum of all the odd numbers from " + a + " to " + n + " = " + sum);
+            //    a = a + 2;
+            //    sum += a;
+            //}
+            #endregion
+            #region Question13
+
+            //int sum = 0;
+            //int a = 0;
+            //int n = GetInteger("Enter a number");
+            //int k = GetInteger("Enter number to be multiples");
+            //while (a <= n)
+            //{
+            //    Console.WriteLine("The sum of all the odd numbers from " + a + " to " + n + " = " + sum);
+            //    a = a + k;
+            //    sum += a;
+            //}
+            #endregion
+            #region Question14
+
+            //int multiple = 1;
+            //int n = GetInteger("Enter the ending number");
+            //for (int i = 1 ; i <= n; i++)
+            //{
+            //    multiple *= i;
+            //}
+            //Console.WriteLine("Factorial 1 to " + n + " = " + multiple);
+            #endregion
+
+            int sem = GetInteger("Enter the number of semesters");
+            int sub = GetInteger("Enter the number of subjects");
+            int i = 1;
+            int m = 1;
+            int sum = 0;
+            
+            while (i < sem)
+            {
+                Console.WriteLine("Semester = " + i);
+                i++;
+
+                while (m <= sub)
+                {
+                    int submar = GetInteger("Enter the mark of the subject " + m);
+                    m++;
+                    sum += submar;
+                    
+                }
+                
+                Console.WriteLine("Total marks for this semester = " + sum);
+                Console.WriteLine("Average mark for this semester = " + sum / sub);
+
+                
+               
+               
+            }
+            
+           
+           
+                
+            
+
+
+
+
+
+
+
+
+
 
 
 
